a0ad0a25b2556d45ded8d5c14c8131b1a66d0c5c
adityayadav176 <ay6598aditya@gmail.com>
2026-01-17 12:21:21 +0530

    some bug fixes

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ window.addEventListener("scroll" , ()=>{
         nav.classList.remove("active");
     }
 });
+
+const form = document.getElementById("contactForm");
+const status = document.getElementById("status");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  fetch("https://formsubmit.co/ajax/ay6598aditya@gmail.com", {
+    method: "POST",
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    status.innerText = "Message sent successfully ✅";
+    conaole.log("send successfull")
+    form.reset();
+  })
+  .catch(error => {
+    status.innerText = "Something went wrong ❌";
+  });
+});
+
+
+
