70b5954a8438019a0878141cc592e783196e2681
adityayadav176 <ay6598aditya@gmail.com>
2026-01-17 21:57:34 +0530

    bug fixes

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ window.addEventListener("scroll" , ()=>{
     }
 });
 
+document.getElementById("hireMeBtn").addEventListener("click", () => {
+
+  // 1️⃣ Download CV
+  const link = document.createElement("a");
+  link.href = "assets/Resume.pdf";
+  link.download = "Aditya_CV.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  // 2️⃣ Scroll to Contact after delay
+  setTimeout(() => {
+    document.getElementById("Contact").scrollIntoView({
+      behavior: "smooth"
+    });
+  }, 200); // delay in ms
+});
+
+
 
 
 
