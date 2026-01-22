6bcb31cfdfcc5a4a2730e1c498f0fb44a139b05e
adityayadav176 <ay6598aditya@gmail.com>
2026-01-22 15:30:52 +0530

    add scoll animation and create portfololio animated

--- script.js
+++ script.js
@@ -27,6 +27,47 @@ document.getElementById("hireMeBtn").addEventListener("click", () => {
     });
   }, 200); // delay in ms
 });
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  const revealPoint = 100;
+
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
+const skills = document.querySelectorAll(".skill");
+let skillsAnimated = false;
+
+function animateSkills() {
+  const skillsSection = document.querySelector(".skills");
+  const sectionTop = skillsSection.getBoundingClientRect().top;
+  const windowHeight = window.innerHeight;
+
+  if (sectionTop < windowHeight - 100 && !skillsAnimated) {
+    skills.forEach(skill => {
+      const circle = skill.querySelector("circle:last-child");
+      const percent = circle.style.getPropertyValue("--percent");
+      const offset = 314 - (314 * percent) / 100;
+
+      circle.style.strokeDashoffset = offset;
+    });
+
+    skillsAnimated = true;
+  }
+}
+
+window.addEventListener("scroll", animateSkills);
+
 
 
 
